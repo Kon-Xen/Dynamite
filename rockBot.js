767085f2eb0f466112e6e94baadebe5e9a99caa5
Konstantinos <kon.xen@gmail.com>
2022-08-09 19:19:16 +0100

    improved opponent Bot.

--- rockBot.js
+++ rockBot.js
@@ -1,6 +1,29 @@
 class Bot {
+
+    dynamites = 100;
+
+    playRandom(max) {
+        let move = Math.floor(Math.random() * max);
+        switch (move) {
+            case 0:
+                return 'R';
+            case 1:
+                return 'P';
+            case 2:
+                return 'S';
+            case 3:
+                if (this.dynamites>=1){
+                    this.dynamites-=1;
+                    return "D";
+                } else { return "W"}
+
+            case 4:
+                return 'W';
+        }
+    }
+
     makeMove(gamestate) {
-        return 'R';
+        return this.playRandom(4);
     }
 }
 
